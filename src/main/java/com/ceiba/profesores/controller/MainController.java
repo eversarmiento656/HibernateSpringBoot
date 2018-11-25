5da37bb08ef209eef59967098d9815a2ff74042d
Ever Sarmiento <eversarmiento656@gmail.com>
2018-11-25 18:37:46 -0500

    Ejemplo de subir una imagen al servidor

--- src/main/java/com/ceiba/profesores/controller/MainController.java
+++ src/main/java/com/ceiba/profesores/controller/MainController.java
@@ -1,9 +1,16 @@
 package com.ceiba.profesores.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +19,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ceiba.profesores.model.Teacher;
 import com.ceiba.profesores.service.ProfesoresService;
@@ -22,6 +32,7 @@ public class MainController {
 
 	@Autowired
 	ProfesoresService profesoresService;
+	private static final String PATH_TEACHERS = "images/teachers/";
 	private static final String ERROR_DATOS = "Error al ingresar los datos";
 	private static final String CORRECTO_CREAR = "Se creó correctamente";
 	private static final String CORRECTO_ELIMINAR = "Se eliminó correctamente";
@@ -75,7 +86,7 @@ public class MainController {
 		if (profesoresService.findTeacherById(id) == null) {
 			return new ResponseEntity<>(TEACHER_NO_ESTA, HttpStatus.NO_CONTENT);
 		}
-        
+
 		Teacher t = profesoresService.findTeacherById(id);
 		t.setAvatar(teacher.getAvatar());
 		t.setName(teacher.getName());
@@ -101,5 +112,55 @@ public class MainController {
 
 		return new ResponseEntity<>(CORRECTO_ELIMINAR, HttpStatus.OK);
 
+	}
+
+	@PostMapping(value = "/profesores/imagenes", headers = "content-type=multipart/form-data")
+	public ResponseEntity<byte[]> unploadImage(@RequestParam("id_teacher") long id,
+			@RequestParam("file") MultipartFile multiPartFile, UriComponentsBuilder uri) {
+
+		if (id == 0) {
+			return new ResponseEntity(ERROR_DATOS, HttpStatus.NO_CONTENT);
+		}
+
+		if (multiPartFile.isEmpty()) {
+			return new ResponseEntity(ERROR_DATOS, HttpStatus.NO_CONTENT);
+		}
+
+		Teacher teacher = profesoresService.findTeacherById(id);
+		if (teacher == null) {
+			return new ResponseEntity(TEACHER_NO_ESTA, HttpStatus.NOT_FOUND);
+		}
+
+		if (!teacher.getAvatar().isEmpty()
+				|| profesoresService.findTeacherById(id).getAvatar() != null) {
+
+			String fileName = teacher.getAvatar();
+			Path path = Paths.get(fileName);
+			File file = path.toFile();
+			if (file.exists()) {
+				file.delete();
+			}
+		}
+
+		try {
+
+			Date date = new Date();
+			SimpleDateFormat simple = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+			String dateName = simple.format(date);
+
+			String fileName = String.valueOf(id) + "- picture -" + dateName + "."
+					+ multiPartFile.getContentType().split("/")[1];
+			
+			teacher.setAvatar(PATH_TEACHERS+fileName);
+			byte[] bytes =  multiPartFile.getBytes();
+			Path path = Paths.get(PATH_TEACHERS, fileName);
+			Files.write(path, bytes);
+			profesoresService.updateTeacher(teacher);
+			return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(bytes);
+
+		} catch (Exception e) {
+
+			return new ResponseEntity(e, HttpStatus.NOT_FOUND);
+		}
 	}
 }
